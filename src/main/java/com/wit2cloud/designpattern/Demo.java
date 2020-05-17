50e28dcdd25f6fc537cbe78c52603b932ca90d32
John-Goo <guhaijiang666@hotmail.com>
2020-05-17 17:26:16 +0800

    设计模式项目——初始化项目

---
+++ src/main/java/com/wit2cloud/designpattern/Demo.java
@@ -0,0 +1,19 @@
+package com.wit2cloud.designpattern;
+/*==========================================================================
+ * Copyright (C) Wit2Cloud Co.,Ltd
+ * All Rights Reserved.
+ * Created By 慧云数字课程
+ ==========================================================================*/
+
+/**
+ * @author John Goo
+ * @version 1.0
+ * @ClassName: Demo
+ * @Desc: 测试环境是否良好
+ * @history v1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello,慧云数字课程！");
+    }
+}
